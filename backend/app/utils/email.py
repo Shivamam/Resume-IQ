f52b1fda793b9b789375867ed6f2ee559eaf79e5
Shivam <shivammishraanil@gmail.com>
2026-04-06 00:58:56 +0530

    Changed Sendgrid email system to personal gmail one.

--- backend/app/utils/email.py
+++ backend/app/utils/email.py
@@ -4,16 +4,30 @@ import os
 
 load_dotenv()
 
+# Account is under review for SendGrid free tier so I am using
+#  my personal gmail account for assignment purpose
+
+# conf = ConnectionConfig(
+#     MAIL_USERNAME="apikey",
+#     MAIL_PASSWORD=os.getenv("SENDGRID_API_KEY"),
+#     MAIL_FROM=os.getenv("MAIL_FROM"),
+#     MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+#     MAIL_PORT=587,
+#     MAIL_SERVER="smtp.sendgrid.net",
+#     MAIL_STARTTLS=True,
+#     MAIL_SSL_TLS=False,
+#     VALIDATE_CERTS=False,
+# )
+
 conf = ConnectionConfig(
-    MAIL_USERNAME="apikey",                        # SendGrid requires this literally
-    MAIL_PASSWORD=os.getenv("SENDGRID_API_KEY"),   # your actual API key goes here
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.sendgrid.net",
+    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
     MAIL_STARTTLS=True,
     MAIL_SSL_TLS=False,
-    VALIDATE_CERTS=False,
+    USE_CREDENTIALS=True,
 )
 
 async def send_otp_email(email: str, otp: str):
